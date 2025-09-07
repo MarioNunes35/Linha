c432ddba1333dd072249c060707364c2c3f67ec3
MarioNunes35 <mariobsnunes@gmail.com>
2025-09-07 10:55:21 -0300

    Update streamlit_baseline_smoothing_lineplot.py

--- streamlit_baseline_smoothing_lineplot.py
+++ streamlit_baseline_smoothing_lineplot.py
@@ -617,7 +617,7 @@ fig.update_layout(
     legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1)
 )
 
-st.plotly_chart(fig, use_container
+st.plotly_chart(fig, use_container_width=True, config={'displaylogo': False})
 
 
 
